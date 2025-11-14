15b20315dcc5db163ddf4f4b55725212f4d14f7e
LeiteyMarsya <leitey123@gmail.com>
2025-11-14 21:39:27 +0800

    Update iss_data table and create iss_analytics table
    Modified the iss_data table structure and added a new iss_analytics table for tracking analytics data.

--- iss_data.sql
+++ iss_data.sql
@@ -1,8 +1,18 @@
 CREATE TABLE iss_data (
-  id SERIAL PRIMARY KEY,
+  id PRIMARY KEY,
   latitude FLOAT,
   longitude FLOAT,
   altitude FLOAT,
   velocity FLOAT,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );  
+
+CREATE TABLE iss_analytics (
+    id                          INTEGER PRIMARY KEY AUTOINCREMENT,
+    period_start                TIMESTAMP NOT NULL,
+    period_end                  TIMESTAMP NOT NULL,
+    max_longitude               REAL,
+    min_longitude               REAL,
+    altitude_changes            INTEGER,
+    altitude_change_magnitude   REAL
+);
