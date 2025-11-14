8c9c5983695dc36b275cadb92b8d7b6a1e25902a
LeiteyMarsya <leitey123@gmail.com>
2025-11-14 22:47:34 +0800

    Fix error reporting settings in get_data.php

--- get_data.php
+++ get_data.php
@@ -1,5 +1,7 @@
 <?php
 // Enable error display temporarily for debugging; remove in production
+ini_set('display_errors', 0);
+error_reporting(0);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -80,4 +82,5 @@ echo json_encode($response);
 
 pg_close($conn);
 ?>
+
 
